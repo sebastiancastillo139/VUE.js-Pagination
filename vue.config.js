60dd2e9eab34b6aa6cd0e5eda0bc2544c014ec4a
Sebastián Castillo <sebastiancastillo1309@gmail.com>
2022-07-09 14:57:57 -0400

    Subida de ejercicio de paginacion

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/proyectotabo/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vuejs-pagination/" : "/",
 })
